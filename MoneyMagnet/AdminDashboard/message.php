7a44a5fa66eea60a40775eea50d5129148297085
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-19 09:39:04 +1000

    added the styling to the popo info message to be better

--- MoneyMagnet/AdminDashboard/message.php
+++ MoneyMagnet/AdminDashboard/message.php
@@ -13,10 +13,12 @@ if (isset($_SESSION['message'])) :
     </head>
 
     <body>
+        <div class="container-message">
         <div id="infoMes" class='infoMessage'>
-            <i class='fas fa-info-circle'></i><strong>Hey!</strong> <?= $_SESSION['message']; ?>
+            <i id="info-icon" class='fas fa-info-circle'></i><strong>Hey!</strong> <?= $_SESSION['message']; ?>
             <i id="close" class="fa-solid fa-x" onclick="closeMessage()"></i>
 
+        </div>
         </div>
 
         <script src="..\Adminjs\message.js"></script>
