fb505db3a992d0d4f56033d2e7f549b08b364647
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-19 19:04:48 +1000

    added some things to the home page to the hero section of the landing page

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -15,12 +15,13 @@
         <div class="container">
             <div class="content-container">
                 <h1 class="title">Money Magnet</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> ornare aliquet nulla in posuere. Fusce sodales elit sit amet mauris iaculis, et mattis felis suscipit.</p>
-            </div>
-            <div class="image-container">
-                <img src="images/house.png" width="500px" height="600px" alt="House Image">
+                <h3>Helps You, <span class="auto-type"></span></h3>
+                <!-- <p class="sentence">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> ornare aliquet nulla in posuere. Fusce sodales elit sit amet mauris iaculis, et mattis felis suscipit.</p> -->
             </div>
         </div>
     </div>
+
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+    <script src="js/home.js"></script>
 </body>
 </html>
--- MoneyMagnet/js/home.js
+++ MoneyMagnet/js/home.js
@@ -1,39 +1,4 @@
-const slider = document.querySelector(".slider");
-const images = document.querySelectorAll(".slider img");
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
 
-let counter = 0;
-
-function showImage(index) {
-    images.forEach((image, i) => {
-        if (i === index) {
-            image.style.opacity = 1;
-        } else {
-            image.style.opacity = 0;
-        }
-    });
-}
-
-function slideRight() {
-    counter = (counter + 1) % images.length;
-    showImage(counter);
-}
-
-function slideLeft() {
-    counter = (counter - 1 + images.length) % images.length;
-    showImage(counter);
-}
-
-// Initial display of the first image
-showImage(counter);
-
-// Add click event listeners to the arrow buttons
-leftArrow.addEventListener("click", slideLeft);
-rightArrow.addEventListener("click", slideRight);
-
-// Automatically change slides every 3 seconds
-setInterval(slideRight, 3500); // Change to the right by default
 
 
 var typed = new Typed(".auto-type", {
