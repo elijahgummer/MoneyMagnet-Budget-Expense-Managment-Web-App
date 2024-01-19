ba4bbd32ec819156cf3313dda91c8cf3e18877c1
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-20 08:15:40 +1000

    changed the font family for the header

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -9,12 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/home.css">
     <title>home</title>
+    
 </head>
 <body>    
     <div id="about">
         <div class="container">
             <div class="content-container">
-                <h1 class="title">Money Magnet</h1>
+                <h2 class="title">Money Magnet</h2>
                 <h3>Helps You, <span class="auto-type"></span></h3>
                 <!-- <p class="sentence">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> ornare aliquet nulla in posuere. Fusce sodales elit sit amet mauris iaculis, et mattis felis suscipit.</p> -->
             </div>
