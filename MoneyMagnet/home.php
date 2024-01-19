b7fdc56832a712bf30bebe042ccebd911c8ca1a6
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-19 13:01:51 +1000

    added some images and text to the main hero section of the home page

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -13,7 +13,13 @@
 <body>    
     <div id="about">
         <div class="container">
-
+            <div class="content-container">
+                <h1 class="title">Money Magnet</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> ornare aliquet nulla in posuere. Fusce sodales elit sit amet mauris iaculis, et mattis felis suscipit.</p>
+            </div>
+            <div class="image-container">
+                <img src="images/house.png" width="500px" height="600px" alt="House Image">
+            </div>
         </div>
     </div>
 </body>
