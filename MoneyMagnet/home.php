620db7abde1d219265af1d3e859f88c131645214
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-22 19:38:36 +1000

    added another p tag

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -49,6 +49,7 @@ require_once("navbar.php");
     <div id="about">
         <h2 class="header">Why Us?</h2>
         <p>stuff</p>
+        <p>stuff</p>
     </div>
 
     <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
