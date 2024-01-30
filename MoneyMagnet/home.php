474f3f48ae244128f9b4fb2a3a596e4ca6842799
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-30 19:50:42 +1000

    added some of the process stuff

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -102,6 +102,12 @@ require_once("navbar.php");
         </div>
     </div>
 
+    <div class="process">
+        <div class="container">
+            
+        </div>
+    </div>
+
     <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
     <script src="js/home.js"></script>
 </body>
