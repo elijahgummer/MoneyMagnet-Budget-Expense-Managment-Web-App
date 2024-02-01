ef24f45e3f4eba203eaded69c70aa29e85ee7a11
Elijah Gummer <elijahgummer5@gmail.com>
2024-02-01 17:43:58 +1000

    added container

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -118,7 +118,9 @@ require_once("navbar.php");
     </div>
 
     <div id="reviews">
-        
+        <div class="container">
+            
+        </div>
     </div>
 
     <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
