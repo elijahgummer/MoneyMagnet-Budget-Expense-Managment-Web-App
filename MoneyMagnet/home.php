cdc285ad94471afe4f119e60259d7355d0e4cd93
Elijah Gummer <elijahgummer5@gmail.com>
2024-02-05 08:08:09 +1000

    changed color and added icons

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -9,6 +9,7 @@ require_once("navbar.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>home</title>
 
 </head>
