a7fd6045f1cf59f877169bb34d920752f470cf18
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-18 11:28:27 +1000

    changed the spacing and indentation

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -18,31 +18,31 @@ require_once("navbar.php");
         <div class="container">
             <div class="content">
                 <div class="content-container">
-                <h2>Money Magnet</h2>
-                <div class="type-animation-text">
-                    <h3 class="typeHeader">Helps You, <span class="auto-type"></span></h3>
-                </div>
-                <div class="fixed-container">
-                    <div class="InputContainer">
-                        <input type="text" name="text" class="input" id="input" placeholder="Search">
-
-                        <label for="input" class="labelforsearch">
-                            <div class="searchIcon">
-                                <i class="fa-solid fa-magnifying-glass"></i>
-                            </div>
-                        </label>
-                        <div class="border"></div>
-
-                        <button class="micButton"><svg viewBox="0 0 384 512" class="micIcon">
-                                <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path>
-                            </svg>
-                        </button>
-
+                    <h2>Money Magnet</h2>
+                    <div class="type-animation-text">
+                        <h3 class="typeHeader">Helps You, <span class="auto-type"></span></h3>
                     </div>
-                    <p class="sentence">Empower your financial journey with<br> Money Magnet, where smart budgeting<br> and seamless expense tracking come <br>together to help you achieve your <br>financial goals and build a secure future.</p>
-                    <button class="getStarted">Get Started<i class="fa-solid fa-arrow-right"></i></button>
+                    <div class="fixed-container">
+                        <div class="InputContainer">
+                            <input type="text" name="text" class="input" id="input" placeholder="Search">
+
+                            <label for="input" class="labelforsearch">
+                                <div class="searchIcon">
+                                    <i class="fa-solid fa-magnifying-glass"></i>
+                                </div>
+                            </label>
+                            <div class="border"></div>
+
+                            <button class="micButton"><svg viewBox="0 0 384 512" class="micIcon">
+                                    <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path>
+                                </svg>
+                            </button>
+
+                        </div>
+                        <p class="sentence">Empower your financial journey with<br> Money Magnet, where smart budgeting<br> and seamless expense tracking come <br>together to help you achieve your <br>financial goals and build a secure future.</p>
+                        <button class="getStarted">Get Started<i class="fa-solid fa-arrow-right"></i></button>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
@@ -51,27 +51,26 @@ require_once("navbar.php");
         <div class="container">
             <div class="content-container">
                 <h3 class="header">Features</h3>
+                <div class="features-container">
+                    <div class="feature-card">
+                        <img src="images/wallet-magnify-glass.png" alt="wallet-magnify-glass">
+                        <h4 class="feature-header">EXPENSE TRACKING</h4>
+                        <p class="content">Effortlessly track your<br> expenses and gain insights<br> into your spending habits<br> with our intuitive expense<br> tracking tools.</p>
+                    </div>
+                    <div class="feature-card">
+                        <img src="images/growth-chart.png" alt="growth-chart">
+                        <h4 class="feature-header-smaller">INVESTMENT INSIGHTS</h4>
+                        <p class="content">Maximize Growth with Investment Insights – Uncover opportunities for smarter investments and financial success.</p>
+                    </div>
+                    <div class="feature-card">
+                        <img src="images/budget-notepad.png" alt="budget-notepad">
+                        <h4 class="feature-header">BUDGETING TOOLS</h4>
+                        <p class="content">Seamlessly manage your<br> finances and achieve your<br> goals with our powerful<br> budgeting tools.</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="features-container">
-            <div class="feature-card">
-                <img src="images/wallet-magnify-glass.png" alt="wallet-magnify-glass">
-                <h4 class="feature-header">EXPENSE TRACKING</h4>
-                <p class="content">Effortlessly track your<br> expenses and gain insights<br> into your spending habits<br> with our intuitive expense<br> tracking tools.</p>
-            </div>
-            <div class="feature-card">
-                <img src="images/growth-chart.png" alt="growth-chart">
-                <h4 class="feature-header-smaller">INVESTMENT INSIGHTS</h4>
-                <p class="content">Maximize Growth with Investment Insights – Uncover opportunities for smarter investments and financial success.</p>
-            </div>
-            <div class="feature-card">
-                <img src="images/budget-notepad.png" alt="budget-notepad">
-                <h4 class="feature-header">BUDGETING TOOLS</h4>
-                <p class="content">Seamlessly manage your<br> finances and achieve your<br> goals with our powerful<br> budgeting tools.</p>
-            </div>
-        </div>
-    </div>
-    <!-- <div id="reviews">
+        <!-- <div id="reviews">
         <div class="container">
             <div class="content-container">
                 <h5 class="header">Reviewed by people</h5>
@@ -128,8 +127,8 @@ require_once("navbar.php");
         </div>
     </div> -->
 
-    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
-    <script src="js/home.js"></script>
+        <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+        <script src="js/home.js"></script>
 </body>
 
 </html>
