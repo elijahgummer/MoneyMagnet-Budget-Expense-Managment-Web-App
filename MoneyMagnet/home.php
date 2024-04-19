0c4b2c699181c518dad66e6eba69c82bd6ad5897
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-19 18:57:25 +1000

    changed the thingo

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -68,6 +68,7 @@ require_once("navbar.php");
                         </linearGradient>
                     </defs>
                 </svg>
+                </div>
                 <div class="content-container">
                     <h3 class="header">Features</h3>
                     <div class="features-container">
@@ -88,7 +89,6 @@ require_once("navbar.php");
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
         </div>
         <div id="reviews">
