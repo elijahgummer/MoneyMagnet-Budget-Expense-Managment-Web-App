492c0e1fb154a3370f04a3e8eb51b41275205b48
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-20 12:55:12 +1000

    changed somthign

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -137,7 +137,6 @@ require_once("navbar.php");
         </div>
         </div>
     </div>
-
         <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
         <script src="js/home.js"></script>
 </body>
