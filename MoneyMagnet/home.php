a1bd4fe6360ddb743ee727ff1040fff5fb860d92
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-20 12:54:16 +1000

    changed the code and what not

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -123,7 +123,7 @@ require_once("navbar.php");
                 </div>
             </div>
         </div>
-        <!--<div id="action">
+        <div id="action">
         <div class="call-to-action-container">
         <img class="call-to-action-image" src="images/callToAction.png" alt="call to action">
         </div>
@@ -140,7 +140,7 @@ require_once("navbar.php");
             
         </div>
         </div>
-    </div> -->
+    </div>
 
         <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
         <script src="js/home.js"></script>
