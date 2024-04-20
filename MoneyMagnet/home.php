e623d29bd2d3f58a50dabeb8c6e07e97bace8f01
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-20 10:28:42 +1000

    changed the code

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -94,7 +94,6 @@ require_once("navbar.php");
                 <div class="content-container">
                     <h5 class="header">Reviewed by People</h5>
                     <h2 class="sub-header">Client's Testimonals</h2>
-                    <p class="content">Discover the positive impact we've made on the our clients by reading through their testimonials. Our clients have experienced our service and results, and they're eager to share their positive experiences with you.</p>
                 </div>
                 <div class="card-container">
                     <div class="card">
