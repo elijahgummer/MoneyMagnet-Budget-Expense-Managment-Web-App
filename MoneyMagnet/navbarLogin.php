fd868cb50f346e602ceaf7fecd773db5a327dd78
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-23 20:24:14 +1000

    changed the hiehgt of the contenct card for the action

--- MoneyMagnet/navbarLogin.php
+++ MoneyMagnet/navbarLogin.php
@@ -29,7 +29,6 @@
     <script>
         var tablinks = document.getElementsByClassName("tab-links");
         var tabcontents = document.getElementsByClassName("tab-contents");
-
         function opentab(tabname) {
             for (tablink of tablinks) {
                 tablink.classList.remove("active-link");
