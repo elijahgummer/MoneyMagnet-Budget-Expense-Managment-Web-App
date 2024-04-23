85ebb5b7271dc6254d073793b7f5fe2945b9c543
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-23 20:23:52 +1000

    changed the hiehgt of the contenct card for the action

--- MoneyMagnet/navbarLogin.php
+++ MoneyMagnet/navbarLogin.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
-
 <body>
     <div id="header">
         <div class="container1">
