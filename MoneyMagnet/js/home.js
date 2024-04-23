9080b161e532b0dec2a0cd213d56b92d2ddbe2fc
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-23 20:21:40 +1000

    changed the whitespace

--- MoneyMagnet/js/home.js
+++ MoneyMagnet/js/home.js
@@ -1,6 +1,3 @@
-
-
-
 var typed = new Typed(".auto-type", {
     strings: ["Take Control", "Achieve Financial Freedom", "Master Your Money", "Secure Your Financial Future", "Budget Like a Pro"],
     typeSpeed: 150,
