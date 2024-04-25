4b405dbcad0f9ee1a0ac4f94f36170a26e46ac63
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-25 12:28:01 +1000

    added a faq area into the web page

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -138,7 +138,7 @@ require_once("navbar.php");
     <div class="FAQ">
         <div class="container">
             <div class="content">
-                
+                <h1>FAQ</h1>
             </div>
         </div>
     </div>
