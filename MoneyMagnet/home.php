70933dd85f03b7185ebf0ea26f31bf5998253d1b
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-25 12:32:13 +1000

    added a p tag

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -140,7 +140,7 @@ require_once("navbar.php");
             <div class="content">
                 <h1>FAQ</h1>
                 <div class="card">
-                    
+                    <p>What is money magnet for?</p>
                 </div>
             </div>
         </div>
