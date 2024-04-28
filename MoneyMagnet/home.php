276fcc8784bf26ef1d45015623db3d6066cb9351
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-28 10:24:50 +1000

    changed the thingo

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -167,14 +167,6 @@ require_once("navbar.php");
                 <i class="fa-brands fa-facebook"></i>
                 <i class="fa-brands fa-x-twitter"></i>
             </div>
-            <ul>
-            <a href="home.php"><li>Home</li></a>
-            <a href="about.php"><li>About</li></a>
-            <a href="features.php"><li>Features</li></a>
-            <a href="process.php"><li>Process</li></a>
-            <a href="reviews.php"><li>Reviews</li></a>
-            <a href="contact.php"><li>Contact</li></a>
-        </ul>
         </div>
     </div>
     </div>
