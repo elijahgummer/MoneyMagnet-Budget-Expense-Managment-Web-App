9c2de5047326f4321d6a6dc2e2a68d95bb3aef92
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-03 11:24:17 +1000

    added some code

--- MoneyMagnet/navbar.php
+++ MoneyMagnet/navbar.php
@@ -32,7 +32,27 @@
             </defs>
         </svg>
     </div>
-    
+    <nav class="navbar">
+      <div class="icon phone-btn">
+        <i class="fa-solid fa-phone"></i>
+      </div>
+      <div class="logo-container">
+        <img class="logo" src="images/logo.png" alt="logo" />
+      </div>
+      <ul id="sidemenu" class="menu-list">
+        <div class="icon cancel-btn">
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="services.html">Services</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="articles.html">Articles</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <button>CALL NOW</button>
+      </ul>
+      <div class="icon menu-btn">
+        <i class="fa-solid fa-bars"></i>
+      </div>
+    </nav>
 </body>
-
 </html>
