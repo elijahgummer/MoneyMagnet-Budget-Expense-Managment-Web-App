4644879d5c03182916f222bd5800ea89ad1f5a53
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-04 17:16:41 +1000

    added the code

--- MoneyMagnet/js/navbar.js
+++ MoneyMagnet/js/navbar.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const icon = document.querySelector(".icon");
-    const navbarList = document.querySelector(".navbar-list");
-
-    icon.addEventListener("click", function () {
-        navbarList.classList.toggle("show");
-    });
-});
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+};
+cancelBtn.onclick = () => {
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
