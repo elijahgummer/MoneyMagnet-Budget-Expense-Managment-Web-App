d5e8f28c80fdf6396ea732ec369272f90e829438
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-16 13:27:29 +1000

    deleted the search bar

--- MoneyMagnet/home.php
+++ MoneyMagnet/home.php
@@ -22,20 +22,6 @@ require_once("navbar.php");
                         <h3 class="typeHeader">Helps You, <span class="auto-type"></span></h3>
                     </div>
                     <div class="fixed-container">
-                        <!-- <div class="InputContainer">
-                            <input type="text" name="text" class="input" id="input" placeholder="Search">
-
-                            <label for="input" class="labelforsearch">
-                                <div class="searchIcon">
-                                    <i class="fa-solid fa-magnifying-glass"></i>
-                                </div>
-                            </label>
-                            <div class="border"></div>
-                            <button class="micButton"><svg viewBox="0 0 384 512" class="micIcon">
-                                    <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path>
-                                </svg>
-                            </button>
-                        </div> -->
                         <button>Get Started</button>
                     </div>
                 </div>
