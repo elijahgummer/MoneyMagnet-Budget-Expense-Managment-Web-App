c236ba5df0db5ea9e6a28b10d21453890fdb9089
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-18 09:29:48 +1000

    remformatted teh js code

--- MoneyMagnet/js/home.js
+++ MoneyMagnet/js/home.js
@@ -1,22 +1,25 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Take Control", "Achieve Financial Freedom", "Master Your Money", "Secure Your Financial Future", "Budget Like a Pro"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
+  strings: [
+    "Take Control",
+    "Achieve Financial Freedom",
+    "Master Your Money",
+    "Secure Your Financial Future",
+    "Budget Like a Pro",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
 
-
-<script>
-                let li = document.querySelectorAll(".faq-text li");
-                for (var i = 0; i < li.length; i++) {
-                    li[i].addEventListener("click", (e) => {
-                        let clickedLi;
-                        if (e.target.classList.contains("question-arrow")) {
-                            clickedLi = e.target.parentElement;
-                        } else {
-                            clickedLi = e.target.parentElement.parentElement;
-                        }
-                        clickedLi.classList.toggle("showAnswer");
-                    });
-                }
-            </script>
+let li = document.querySelectorAll(".faq-text li");
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", (e) => {
+    let clickedLi;
+    if (e.target.classList.contains("question-arrow")) {
+      clickedLi = e.target.parentElement;
+    } else {
+      clickedLi = e.target.parentElement.parentElement;
+    }
+    clickedLi.classList.toggle("showAnswer");
+  });
+}
