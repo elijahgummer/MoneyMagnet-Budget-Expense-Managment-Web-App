99d18ad8c1a8389fe1760d24cebbf4d93958c23c
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-17 19:14:45 +1000

    added a button

--- MoneyMagnet/navbar.php
+++ MoneyMagnet/navbar.php
@@ -2,32 +2,39 @@
 <html lang="en" dir="ltr">
 
 <head>
-    <meta charset="utf-8">
-    <title>Responsive Navbar</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <meta charset="utf-8">
+  <title>Responsive Navbar</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/navbar.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 
 <body>
-    <nav>
-      <div class="logo-container">
-        <img class="logo" src="images/logo.png" alt="logo" />
+  <nav>
+    <div class="logo-container">
+      <img class="logo" src="images/logo.png" alt="logo" />
+    </div>
+    <ul id="sidemenu" class="menu-list">
+      <div class="icon cancel-btn">
+        <i class="fa-solid fa-xmark"></i>
       </div>
-      <ul id="sidemenu" class="menu-list">
-        <div class="icon cancel-btn">
-          <i class="fa-solid fa-xmark"></i>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="features.html">Features</a></li>
+      <li><a href="reviews.html">Reviews</a></li>
+      <li><a href="action.html">Action</a></li>
+      <li><a href="FAQ.html">FAQ</a></li>
+      <button>
+        Sign up
+        <div class="arrow-wrapper">
+          <div class="arrow"></div>
         </div>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="features.html">Features</a></li>
-        <li><a href="reviews.html">Reviews</a></li>
-        <li><a href="action.html">Action</a></li>
-        <li><a href="FAQ.html">FAQ</a></li>
-      </ul>
-      <div class="icon menu-btn">
-        <i class="fa-solid fa-bars"></i>
-      </div>
-    </nav>
-    <script src="js/navbar.js"></script>
+      </button>
+    </ul>
+    <div class="icon menu-btn">
+      <i class="fa-solid fa-bars"></i>
+    </div>
+  </nav>
+  <script src="js/navbar.js"></script>
 </body>
+
 </html>
