116f0c09a1d2f9a8c960e35e38eb7a37aca74cf4
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-26 16:59:17 +1000

    changed the js to wait until the DOM HAS fully loaded

--- MoneyMagnet/js/navbar.js
+++ MoneyMagnet/js/navbar.js
@@ -1,11 +1,18 @@
-const navbar = document.querySelector(".navbar");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = () => {
-  navbar.classList.add("show");
-  menuBtn.classList.add("hide");
-};
-cancelBtn.onclick = () => {
-  navbar.classList.remove("show");
-  menuBtn.classList.remove("hide");
-};
+document.addEventListener("DOMContentLoaded", function() {
+  const menuBtn = document.querySelector('.menu-btn');
+  const cancelBtn = document.querySelector('.cancel-btn');
+  const menuList = document.querySelector('.menu-list');
+  const body = document.querySelector('body');
+
+  menuBtn.onclick = function() {
+      menuList.classList.add('show');
+      menuBtn.classList.add('hide');
+      body.classList.add('disabled');
+  }
+
+  cancelBtn.onclick = function() {
+      menuList.classList.remove('show');
+      menuBtn.classList.remove('hide');
+      body.classList.remove('disabled');
+  }
+});
